e8212670c78a78e460e8f9b80443209b77abb9bd
poo <poo@u.c>
2023-05-07 23:00:26 +1000

    Added banner

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	b64 "encoding/base64"
 	"io"
 	"log"
 	"os"
@@ -9,6 +10,15 @@ import (
 	"github.com/gliderlabs/ssh"
 	"github.com/akamensky/argparse"
 )
+
+const BANNER = `
+ICAgICAgICAgICAgICAgICAgICAgX19fXyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+ICAgX18KICAgX19fXyBfX19fXyAgICAgICAvIF9fL19fXyAgX19fX19fICAgICAgX19fX19fIF9f
+X19fX19fX18vIC8KICAvIF9fIGAvIF9fIFwgICAgIC8gL18vIF9fIFwvIF9fXy8gfCAvfCAvIC8g
+X18gYC8gX19fLyBfXyAgLyAKIC8gL18vIC8gL18vIC8gICAgLyBfXy8gL18vIC8gLyAgIHwgfC8g
+fC8gLyAvXy8gLyAvICAvIC9fLyAvICAKIFxfXywgL1xfX19fL19fX18vXy8gIFxfX19fL18vICAg
+IHxfXy98X18vXF9fLF8vXy8gICBcX18sXy8gICAKL19fX18vICAgICAvX19fX18vICAgICAgICAg
+ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAK`
 
 type Arguments struct {
 	port string
@@ -23,27 +33,27 @@ func get_args() Arguments {
 	port := parser.String("p", "port",
 		&argparse.Options{
 			Required: false,
-			Help: "The port to run the SSH server on",
+			Help: "The port to run the SSH server on (Optional)",
 			Default: "22",
 		})
 
 	iface := parser.String("i", "interface",
 		&argparse.Options{
 			Required: false,
-			Help: "The interface to run the SSH server on",
+			Help: "The interface to run the SSH server on (Optional)",
 			Default: "0.0.0.0",
 		})
 
 	userName := parser.String("U", "username",
 	&argparse.Options{
 		Required: true,
-		Help: "The username for authentication to the SSH server",
+		Help: "The username for authentication to the SSH server (Required)",
 	})
 
 	passWord := parser.String("P", "password",
 	&argparse.Options{
 		Required: true,
-		Help: "The password for authentication to the SSH server",
+		Help: "The password for authentication to the SSH server (Required)",
 	})
 
 	argErr := parser.Parse(os.Args)
@@ -61,6 +71,9 @@ func get_args() Arguments {
 
 func main() {
 
+	banner, _ := b64.StdEncoding.DecodeString(BANNER)
+	fmt.Printf("%s\n", banner)
+	
 	arguments := get_args()
 
 	log.Printf("starting SSH server on port %s:%s...", arguments.iface, arguments.port)
